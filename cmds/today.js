05ad25aee572aadab718c9b431d796b0d4a85f41
Dustin <druskell@gmail.com>
2019-01-04 12:15:04 -0800

    Jooooooker

--- cmds/today.js
+++ cmds/today.js
@@ -1,3 +1,14 @@
 module.exports = (args) => {
-  console.log('today is sunny')
+
+  var exec = require('child_process').exec, child;
+
+  child = exec('cat *.js bad_file | wc -l',
+      function (error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+               console.log('exec error: ' + error);
+          }
+      });
+   // child();
 }
