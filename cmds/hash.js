560074b7069cc09f14fef49c921cd2358f87b984
Dustin <druskell@gmail.com>
2019-01-07 11:42:45 -0800

    testing

--- cmds/hash.js
+++ cmds/hash.js
@@ -31,7 +31,7 @@ ${message()}`
     let hashToSubmit =()=> {
       shell.exec(`echo "commit ${byteNum}${commitMessage}"`).exec(`sha1sum`)
     }
-    let hash =()=> shell.exec(`echo "${commitMessage}"`).exec(`git hash-object -t commit -w --stdin`)
+    let hash =()=> shell.exec(`echo "${commitMessage}"`).exec(`git hash-object -t commit --stdin`)
     shell.exec(`git reset --hard ${hash()}`)
     shell.echo(`${testForZeroes()}`)
   }
