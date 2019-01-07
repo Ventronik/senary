6ae27e6b2f8ec9513e9f97ae28ff3e9559345586
Dustin <druskell@gmail.com>
2019-01-07 00:02:53 -0800

    testing

--- cmds/hash.js
+++ cmds/hash.js
@@ -19,9 +19,9 @@ committer Dan Kozlowski <koz@planetscale.com> 1545187366 +0500
 
 ${message()}`
   commit()
-let byteNum = shell.exec(`echo "${commit}"`).exec('wc -c').exec(`tr -d '\n'`)
-let hashToSubmit = shell.exec(`echo "commit ${byteNum}${commit}"`).exec(`sha1sum`)
-let hash = shell.exec(`echo "${commit}"`).exec(`git hash-object -t commit -w --stdin`)
+let byteNum = shell.exec(`echo "${commit()}"`).exec('wc -c').exec(`tr -d '\n'`)
+let hashToSubmit = shell.exec(`echo "commit ${byteNum}${commit()}"`).exec(`sha1sum`)
+let hash = shell.exec(`echo "${commit()}"`).exec(`git hash-object -t commit -w --stdin`)
 
 shell.exec(`git reset --hard ${hash}`)
 
