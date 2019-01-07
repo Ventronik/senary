b69cbbf2ac0dbc472d6858986c840be44afcd50b
Dustin <druskell@gmail.com>
2019-01-07 13:03:17 -0800

    testing

--- cmds/hash.js
+++ cmds/hash.js
@@ -6,16 +6,16 @@ module.exports = (args) => {
   shell.config.silent = true;
 
   let attemptCounter = 0
-  let userName = shell.exec('git config --get user.name').exec(`tr -d '\n'`)
-  let userEmail = shell.exec('git config --get user.email').exec(`tr -d '\n'`)
+  // let userName = shell.exec('git config --get user.name').exec(`tr -d '\n'`)
+  // let userEmail = shell.exec('git config --get user.email').exec(`tr -d '\n'`)
   let message =()=> `This is my commit message, attempt ${attemptCounter}`
   let writeTree = shell.exec(`git write-tree`).exec(`tr -d '\n'`)
   let revParse = shell.exec(`git rev-parse HEAD`).exec(`tr -d '\n'`)
   message()
   let commit =()=>`tree ${writeTree}
 parent ${revParse}
-author Dan Kozlowski <koz@planetscale.com> 1545187366 +0500
-committer Dan Kozlowski <koz@planetscale.com> 1545187366 +0500
+author Dan Kozlowski <koz@planetscale.com>  1545187366 +0500
+committer Dan Kozlowski <koz@planetscale.com>  1545187366 +0500
 
 ${message()}`
 
@@ -36,6 +36,8 @@ ${message()}`
     shell.echo(`${testForZeroes()}`)
   }
   shell.config.silent = silentState
+    shell.echo(`${commit()}`)
+
 
   shell.exec('git push origin master')
 }
