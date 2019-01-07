76cb302c36ea5bb06639e766ae34f8c62ea05b45
Dustin <druskell@gmail.com>
2019-01-06 23:32:39 -0800

    refactoring the code so content can be updated as it loops

--- cmds/today.js
+++ cmds/today.js
@@ -1,13 +1,23 @@
 module.exports = (args) => {
+  var shell = require('shelljs')
+  let counter = 0
 
-  var exec = require('child_process').exec, child;
+  let greeting =()=> shell.echo(`hello world ${counter}`)
 
-  child = exec('cat *.js bad_file | wc -l',
-      function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          console.log('stderr: ' + stderr);
-          if (error !== null) {
-               console.log('exec error: ' + error);
-          }
-      });
+  while(counter < 10) {
+    counter++
+    greeting()
+  }
+
+
+  // var exec = require('child_process').exec, child;
+  //
+  // child = exec('cat *.js bad_file | wc -l',
+  //     function (error, stdout, stderr) {
+  //         console.log('stdout: ' + stdout);
+  //         console.log('stderr: ' + stderr);
+  //         if (error !== null) {
+  //              console.log('exec error: ' + error);
+  //         }
+  //     });
 }
