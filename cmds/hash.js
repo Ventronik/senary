1088f2a9712399b1215b03eda55de911b542d340
Dustin <druskell@gmail.com>
2019-01-07 22:41:12 -0800

    testing output

--- cmds/hash.js
+++ cmds/hash.js
@@ -1,17 +1,19 @@
 module.exports = (args) => {
   var shell = require('shelljs');
-  let spawn = require('child_process').spawn
+  let spawn = require('child_process').spawn;
 
   var silentState = shell.config.silent;
   shell.config.silent = true;
 
-  let userName = shell.exec('git config --get user.name').exec(`tr -d '\n'`)
-  let userEmail = shell.exec('git config --get user.email').exec(`tr -d '\n'`)
-  let writeTree = shell.exec(`git write-tree`).exec(`tr -d '\n'`)
-  let commitHash =()=> shell.exec('git rev-parse HEAD').exec(`tr -d '\n'`)
+  let userName = shell.exec('git config --get user.name').exec(`tr -d '\n'`);
+  let userEmail = shell.exec('git config --get user.email').exec(`tr -d '\n'`);
+  let writeTree = shell.exec(`git write-tree`).exec(`tr -d '\n'`);
+  let commitHash =()=> shell.exec('git rev-parse HEAD').exec(`tr -d '\n'`);
 
-  let attemptCounter = 0
-  let message =()=> `This is my commit message, attempt ${attemptCounter}`
+  let attemptCounter = 0;
+  let message =()=> `This is my commit message, attempt ${attemptCounter}`;
+  let hash;
+  let testForZeroes ='';
 
 // KEEP FORMATTING THIS WAY! New lines in string literals is inerpreted as new lines, and tabs are registered as tabs.
   let commit =()=>`tree ${writeTree}
@@ -19,25 +21,21 @@ parent ${commitHash()}
 author ${userName} <${userEmail}> 1545187366 +0500
 committer ${userName} <${userEmail}> 1545187366 +0500
 
-${message()}`
-  let hash
+${message()}`;
 
-
-let testForZeroes =''
-
-  // while(attemptCounter < 5){
-  while(testForZeroes != '00'){
+  while(attemptCounter < 5){
+  // while(testForZeroes != '00'){
     attemptCounter++
-    let commitMessage = commit()
+    let commitMessage = commit();
       // let hashToSubmit =()=> shell.exec(`echo "commit ${byteNum}${commitMessage}"`).exec(`sha1sum`)
-    let hashMaker =()=> shell.exec(`echo "${commitMessage}"`).exec(`git hash-object -t commit -w --stdin`)
-    hash = hashMaker()
+    let hashMaker =()=> shell.exec(`echo "${commitMessage}"`).exec(`git hash-object -t commit -w --stdin`);
+    hash = hashMaker();
     // testForZeroes = shell.exec('git rev-parse HEAD').exec(`tr -d '\n'`).slice(0,1)
-    testForZeroes = hash.slice(0,2)
+    testForZeroes = hash.slice(0,2);
       shell.echo(`LOOK AT ME!!!! ${hash}`)
   }
-  shell.config.silent = silentState
-  // shell.exec(`git reset --hard ${hash}`)
+  // shell.exec(`git reset --hard ${hash}`);
+  shell.config.silent = silentState;
 
-  // shell.exec('git push origin master')
+  // shell.exec('git push origin master');
 }
