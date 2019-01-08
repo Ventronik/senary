2cfcb8bdeb0ba9e8502475eb78920b6ed7a19d64
Dustin <druskell@gmail.com>
2019-01-07 22:25:26 -0800

    testing output

--- cmds/hash.js
+++ cmds/hash.js
@@ -31,7 +31,7 @@ let testForZeroes =''
     shell.config.silent = silentState
       // let hashToSubmit =()=> shell.exec(`echo "commit ${byteNum}${commitMessage}"`).exec(`sha1sum`)
     let hash =()=> shell.exec(`echo "${commitMessage}"`).exec(`git hash-object -t commit -w --stdin`)
-    shell.exec(`git reset --hard ${hash()}`)
+    // shell.exec(`git reset --hard ${hash()}`)
       shell.echo(`LOOK AT ME!!!! ${hash()}`)
   }
 
